9d3300e39177b2d6a7d3263a7170eed7c5220398
Iris-Jiang311 <123715809+Iris-Jiang311@users.noreply.github.com>
2023-12-11 22:33:32 +0100

    Add files via upload

---
+++ Day 7: Regular Expressions II.js
@@ -0,0 +1,57 @@
+// Task
+
+// Complete the function in the editor below by returning a RegExp object, , that matches any string  satisfying both of the following conditions:
+
+// String  starts with the prefix Mr., Mrs., Ms., Dr., or Er.
+// The remainder of string  (i.e., the rest of the string after the prefix) consists of one or more upper and/or lowercase English alphabetic letters (i.e., [a-z] and [A-Z]).
+// Sample Input 1
+
+// Mrs.Y
+// Sample Output 1
+
+// true
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    let re = /^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z])+$/
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(!!s.match(re));
+}
